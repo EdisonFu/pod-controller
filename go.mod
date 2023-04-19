6bdd671ca49eb448b6bb83b37d3ce8b8fcd3fb57
傅真东(技术中心/服务研发部) <zhendong.fu@yintech.cn>
2023-04-19 16:39:15 +0800

    add k8s client-go

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/alecthomas/log4go v0.0.0-20180109082532-d146e6b86faa
 	github.com/kr/pretty v0.3.1 // indirect
 	go.uber.org/automaxprocs v1.5.2
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	k8s.io/api v0.27.1
+	k8s.io/apimachinery v0.27.1
 )
--- handler/controller.go
+++ handler/controller.go
@@ -1,5 +1,14 @@
 package handler
 
+import (
+	l4g "github.com/alecthomas/log4go"
+	coreV1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
 //通过预测值计算所需pod数量
 func CalcPodNum(predict int64) int {
 	//计算pod数量
@@ -11,6 +20,34 @@ func CalcPodNum(predict int64) int {
 }
 
 //向k8s调度器发送请求，把pod数量调整到指定值
-func SendPodNum(podNum int) error {
-	return nil
+func SendPodNum(podNum int) (err error) {
+	//获取k8s的restClient
+	config, err := clientcmd.BuildConfigFromFlags("", "/root/.kube/config")
+	if err != nil {
+		l4g.Error("build config error:%v", err)
+		return err
+	}
+	config.APIPath = "api"
+	config.GroupVersion = &coreV1.SchemeGroupVersion
+	config.NegotiatedSerializer = scheme.Codecs
+
+	restClient, err := rest.RESTClientFor(config)
+	if err != nil {
+		l4g.Error("create rest client error:%v", err)
+		return err
+	}
+
+	//把deployment的副本数改为podNum
+	deployment := &appsv1.Deployment{}
+	err = restClient.Get().Namespace("default").Resource("deployments").Name("pod-controller").VersionedParams(&metaV1.GetOptions{}, scheme.ParameterCodec).Do(context.TODO()).Into(deployment)
+	if err != nil {
+		l4g.Error("get deployment error:%v", err)
+		return err
+	}
+	deployment.Spec.Replicas = &podNum
+	_, err = restClient.Put().Namespace("default").Resource("deployments").Name("pod-controller").Body(deployment).Do(context.TODO()).Get()
+	if err != nil {
+		l4g.Error("put deployment error:%v", err)
+	}
+	return err
 }
